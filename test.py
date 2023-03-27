37cedf475155d8d1c6c3503ef123ace48b581f79
haileKemer <hailemariam@kemercode.com>
2023-03-27 22:26:43 +0300

    add tests

--- test.py
+++ test.py
@@ -2,16 +2,39 @@ import unittest
 from string_functions import *
 
 class TestToUpper(unittest.TestCase):
-    # write your code here
-    pass
+    def test_upper(self):
+        self.assertEqual(to_upper('hello'), 'HELLO')
+        self.assertEqual(to_upper("hello"), "HELLO")
+        self.assertEqual(to_upper("HELLO"), "HELLO")
+        self.assertEqual(to_upper("Hello"), "HELLO")
+        self.assertEqual(to_upper(""), "")
+        self.assertEqual(to_upper(" "), " ")
+        self.assertEqual(to_upper("123"), "123")
+        self.assertEqual(to_upper("hello world"), "HELLO WORLD")
+
 
 class TestToLower(unittest.TestCase):
-    # Write you code here
-    pass
+    def test_lower(self):
+        self.assertEqual(to_lower('HELLO'), 'hello')
+        self.assertEqual(to_lower("HELLO"), "hello")
+        self.assertEqual(to_lower("hello"), "hello")
+        self.assertEqual(to_lower("Hello"), "hello")
+        self.assertEqual(to_lower(""), "")
+        self.assertEqual(to_lower(" "), " ")
+        self.assertEqual(to_lower("123"), "123")
+        self.assertEqual(to_lower("Hello World"), "hello world")
+
 
 class TestCapitalize(unittest.TestCase):
-    # Write your code here
-    pass
+    def test_upper(self):
+        self.assertEqual(capitalize('hello'), 'Hello')
+        self.assertEqual(capitalize("hello"), "Hello")
+        self.assertEqual(capitalize("HELLO"), "Hello")
+        self.assertEqual(capitalize("Hello"), "Hello")
+        self.assertEqual(capitalize(""), "")
+        self.assertEqual(capitalize(" "), " ")
+        self.assertEqual(capitalize("123"), "123")
+        self.assertEqual(capitalize("hello world"), "Hello world")
 
 if __name__ == '__main__':
     unittest.main()
